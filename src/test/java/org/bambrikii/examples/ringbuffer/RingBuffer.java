dddaaf7beefb2fce256b86774d21216c409cebc8
Ալեքսանդր Առաքելյան <bambrikii@gmail.com>
2017-01-25 21:58:05 +0300

    - wrapped tests, - empty, full flags.

--- src/test/java/org/bambrikii/examples/ringbuffer/RingBuffer.java
+++ src/test/java/org/bambrikii/examples/ringbuffer/RingBuffer.java
@@ -14,39 +14,40 @@ public class RingBuffer<T> implements RingBufferable<T> {
 	private int pos;
 	private int tail;
 	private int len;
+	private boolean full;
+	private boolean empty;
 
 	public RingBuffer(int size) {
 		Class<T> cls = ((Class<T>) ((TypeVariable) getClass().getTypeParameters()[0]).getBounds()[0]);
 		arr = (T[]) Array.newInstance(cls, size);
 		pos = -1;
 		len = size;
-		tail = len;
+		tail = len - 1;
+		empty = true;
+		full = false;
 	}
 
 	@Override
 	public boolean push(T elem) {
-		int nextPos = calcNextPos();
-		if (nextPos > len - 1) {
-			if (nextPos == tail) {
-				return false;
-			}
-			nextPos = nextPos % (len - 1) - 1;
-			if (nextPos < tail) {
-				return push(elem, nextPos);
-			} else if (nextPos > tail) {
-				return push(elem, nextPos);
-			}
-		} else if (nextPos < tail) {
-			return push(elem, nextPos);
-		} else if (nextPos > tail) {
-			return push(elem, nextPos);
+		if (full) {
+			return false;
 		}
-
-		return false;
+		if (pos == tail && !empty) {
+			full = true;
+			return false;
+		}
+		int nextPos = calcNextPos();
+		empty = false;
+		full = (nextPos == tail);
+		return push(elem, nextPos);
 	}
 
 	private int calcNextPos() {
-		return pos + 1;
+		int nextPos = pos + 1;
+		if (nextPos > len - 1) {
+			nextPos = 0;
+		}
+		return nextPos;
 	}
 
 	private boolean push(T elem, int newPos) {
@@ -57,10 +58,15 @@ public class RingBuffer<T> implements RingBufferable<T> {
 
 	@Override
 	public T pop() {
-		if (tail == pos) {
+		if (empty) {
+			return null;
+		}
+		if (tail == pos && !full) {
 			return null;
 		}
 		int nextTail = calcNextTail();
+		empty = (nextTail == pos);
+		full = false;
 		return pop(nextTail);
 	}
 
--- src/test/java/org/bambrikii/examples/ringbuffer/RingBufferTest.java
+++ src/test/java/org/bambrikii/examples/ringbuffer/RingBufferTest.java
@@ -26,11 +26,11 @@ public class RingBufferTest {
 		assertEquals(Integer.valueOf(1), buf.pop());
 		assertEquals(Integer.valueOf(2), buf.pop());
 		assertEquals(Integer.valueOf(3), buf.pop());
-		assertEquals(null, buf.pop());
+		assertNull(buf.pop());
 	}
 
 	@Test
-	public void testPop2() {
+	public void testPopWrapped() {
 		RingBuffer<Integer> buf = new RingBuffer<>(3);
 		buf.push(1);
 		buf.push(2);
@@ -38,17 +38,39 @@ public class RingBufferTest {
 		assertEquals(Integer.valueOf(2), buf.pop());
 		buf.push(3);
 		assertEquals(Integer.valueOf(3), buf.pop());
-		assertEquals(null, buf.pop());
+		assertNull(buf.pop());
 		buf.push(4);
 		assertEquals(Integer.valueOf(4), buf.pop());
 		buf.push(5);
 		assertEquals(Integer.valueOf(5), buf.pop());
 		buf.push(6);
 		assertEquals(Integer.valueOf(6), buf.pop());
+		assertNull(buf.pop());
 		buf.push(7);
 		assertEquals(Integer.valueOf(7), buf.pop());
 		buf.push(8);
 		assertEquals(Integer.valueOf(8), buf.pop());
-		assertEquals(null, buf.pop());
+		assertNull(buf.pop());
+	}
+
+	@Test
+	public void testPushWrapped() {
+		RingBuffer<Integer> buf = new RingBuffer<>(3);
+		assertTrue(buf.push(1));
+		assertTrue(buf.push(2));
+		assertTrue(buf.push(3));
+		assertFalse(buf.push(4));
+		assertEquals(Integer.valueOf(1), buf.pop());
+		assertEquals(Integer.valueOf(2), buf.pop());
+		assertEquals(Integer.valueOf(3), buf.pop());
+		assertNull(buf.pop());
+		assertTrue(buf.push(4));
+		assertTrue(buf.push(5));
+		assertTrue(buf.push(6));
+		assertFalse(buf.push(7));
+		assertEquals(Integer.valueOf(4), buf.pop());
+		assertEquals(Integer.valueOf(5), buf.pop());
+		assertEquals(Integer.valueOf(6), buf.pop());
+		assertNull(buf.pop());
 	}
 }
